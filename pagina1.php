aff7a483738246e367d0c65765ad482022e68e5b
gabrielaramburu <gabrielaramburu@gmail.com>
2023-05-15 16:37:41 -0300

    first commit

---
+++ pagina1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Pagina de prueba</h1>
+
+<p>Hola, hoy es </p>
+<?php
+echo date('h:i:s A');
+?>
+
+</body>
+</html>
---
+++ pagina2.php
@@ -0,0 +1,1 @@
+<?php echo '<h1>Devuelvo un titulo</h1>' ?>
