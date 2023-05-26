f4cac6d662266d3f82937e4d3a94124e888cfb08
gabrielaramburu <gabrielaramburu@gmail.com>
2023-05-26 12:28:43 -0300

    ejemplo

--- modelo/6_bucleForConObjeto.php
+++ modelo/6_bucleForConObjeto.php
@@ -2,7 +2,9 @@
 
 include 'Usuario.php';
 
-$personas = array (new Usuario("pepe",18));
+$personas = array (
+    new Usuario("pepe",18),
+    new Usuario("maria",18));
 
 for ($i = 0;$i < count($personas); $i++) {
     echo $personas[$i]->getNombre() . " " . $personas[$i]->getEdad();
